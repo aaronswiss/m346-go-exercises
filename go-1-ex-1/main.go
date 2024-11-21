0b077b85c227f6825ab3628cbe6bd8e8991493d6
Aaron Ettlin <aaron.ettlin@sluz.ch>
2024-11-21 15:44:15 +0100

    Aufgabe 1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -3,6 +3,15 @@ package main
 import "fmt"
 
 func main() {
+	firstName := "Aaron"
+	lastName := "Ettlin"
+	dayOfBirth := 18
+	monthOfBirth := 04
+	yearOfBirth := 2007
+	numberOfSiblings := 1
+	heightInMeters := 1.81
+	zodiacSign := '\u2648'
+
 	// TODO: Declare and initialize the variables being used in the output!
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
