828f92048c103cbe6754acaceeacdabc4e414795
Aaron Ettlin <aaron.ettlin@sluz.ch>
2024-11-28 15:25:53 +0100

    Aufgabe 2.2

--- go-2-ex-2/main.go
+++ go-2-ex-2/main.go
@@ -3,13 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var fibs = []int{1, 1, 0, 0, 0}
+	var fibs = []int{1, 1}
 
-	fibs[2] = fibs[0] + fibs[1]
-	// TODO: correct up to index 4 using direct element access
-
-	fibs = append(fibs, 0) // TODO: replace 0 with the next Fibonacci number
-	// TODO: compute three more Fibonacci numbers and append them
+	for i := 2; i < 9; i++ {
+		nextFib := fibs[i-1] + fibs[i-2]
+		fibs = append(fibs, nextFib)
+	}
 
 	fmt.Println(fibs) // expected output: [1 1 2 3 5 8 13 21 34]
 }
