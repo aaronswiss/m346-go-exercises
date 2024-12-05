ba96d4839c117bc48942fc741231f87013f5d787
Aaron Ettlin <aaron.ettlin@sluz.ch>
2024-12-05 15:36:16 +0100

    Aufgabe 3.3

--- go-3-ex-3/main.go
+++ go-3-ex-3/main.go
@@ -6,5 +6,19 @@ const (
 )
 
 func main() {
-	// TODO: Implement FizzBuzz using a for loop from Lower to Upper.
+	for i := Lower; i <= Upper; i++ {
+		switch {
+		case i%3 == 0 && i%5 == 0:
+			println("FizzBuzz")
+			fallthrough
+		case i%3 == 0:
+			println("Fizz")
+			fallthrough
+		case i%5 == 0:
+			println("Buzz")
+			fallthrough
+		default:
+			println(i)
+		}
+	}
 }
