da37a4584aa881355bfc18be86df85f90ff4498c
Aaron Ettlin <aaron.ettlin@sluz.ch>
2024-12-05 16:26:32 +0100

    Aufgabe 4.1 angefangen

--- go-4-ex-1/main.go
+++ go-4-ex-1/main.go
@@ -1,5 +1,10 @@
 package main
 
+func computeGrade(score int, maxscore int) float64 {
+	grade := (float64(score)/float64(maxscore))*5 + 1
+	return grade
+}
+
 // TODO: implement the function computeGrade
 
 func main() {
